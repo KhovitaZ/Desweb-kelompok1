4e025f862a27495d92831786303cd95960d06fb2
nasyaimutdanlucu <nasyaadea@gmail.com>
2024-11-30 23:17:21 +0800

    baikin

--- Project_Desweb/src/components/Navbar/Navbar.jsx
+++ Project_Desweb/src/components/Navbar/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
   return (
     <div className="fixed h-[75px] md:h-[75px] lg:h-[84px] w-full bg-[#55B0F1] flex flex-row justify-between items-center top-0 z-40">
       <div className="flex flex-row justify-start items-center">
-        <div className="w-[70px] md:w-[100px] lg:w-[150px]">
+        <div className="w-[50px] sm:w-[70px] md:w-[100px] lg:w-[150px]">
           <img src={Logo}/>
         </div>
         <div>
-          <h1 className="font-semibold text-[14px] md:text-[17px] lg:text-[24px] xl:text-[28px]">Startup Academy</h1>
+          <h1 className="font-semibold text-[12px] sm:text-[14px] md:text-[17px] lg:text-[24px] xl:text-[28px]">Startup Academy</h1>
         </div>
-        <div className="hidden sm:flex text-[16px] md:text-[12px] lg:text-[16px] gap-2 md:gap-2 lg:gap-7 xl:gap-10 justify-center items-center pl-5">
+        <div className="hidden sm:flex text-[10px] sm:text-[12px] md:text-[14px] lg:text-[16px] gap-2 md:gap-4 lg:gap-7 xl:gap-10 justify-center items-center pl-3 sm:pl-5">
           <a href="#homePage">Home</a>
           <a href="#program">Program</a>
           <a href="#event">Event</a>
@@ -24,16 +24,16 @@ const Navbar = () => {
         </div>
       </div>
       
-      <div className="flex flex-row justify-end items-center gap-3 mr-4">
+      <div className="flex flex-row justify-end items-center gap-2 sm:gap-3 md:gap-4 mr-2 sm:mr-4">
         <DarkMode />
         <Link to="/login">
-          <button className="h-[40px] w-[90px] flex justify-center items-center bg-gradient-to-r from-[#FFEEAE] to-[#FDD22F] text-black font-bold py-2 px-4 rounded-[8px] hover:from-yellow-500 hover:to-yellow-600">
-            <p>Masuk</p>
+          <button className="h-[35px] sm:h-[40px] w-[80px] sm:w-[90px] flex justify-center items-center bg-gradient-to-r from-[#FFEEAE] to-[#FDD22F] text-black font-bold py-2 sm:py-2 px-3 sm:px-4 rounded-[6px] sm:rounded-[8px] hover:from-yellow-500 hover:to-yellow-600">
+            <p className="text-[10px] sm:text-[12px] md:text-[14px]">Masuk</p>
           </button>
         </Link>
         <Link to="/daftar">
-          <button className="h-[40px] w-[90px] flex justify-center items-center bg-gradient-to-r from-[#FFEEAE] to-[#FDD22F] text-black font-bold py-2 px-4 rounded-[8px] hover:from-yellow-500 hover:to-yellow-600">
-            <p>Daftar</p>
+          <button className="h-[35px] sm:h-[40px] w-[80px] sm:w-[90px] flex justify-center items-center bg-gradient-to-r from-[#FFEEAE] to-[#FDD22F] text-black font-bold py-2 sm:py-2 px-3 sm:px-4 rounded-[6px] sm:rounded-[8px] hover:from-yellow-500 hover:to-yellow-600">
+            <p className="text-[10px] sm:text-[12px] md:text-[14px]">Daftar</p>
           </button>
         </Link>
       </div>
