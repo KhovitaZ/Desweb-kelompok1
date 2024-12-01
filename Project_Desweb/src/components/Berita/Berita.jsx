11bff4dcd267e8b18ec49cc829412410e11a3a4e
nasyaimutdanlucu <nasyaadea@gmail.com>
2024-12-01 23:05:52 +0800

    dm

--- Project_Desweb/src/components/Berita/Berita.jsx
+++ Project_Desweb/src/components/Berita/Berita.jsx
@@ -43,18 +43,18 @@ const ProgramData = [
 
 const Berita = () => {
   return (
-    <div id="berita" className="mt-14 mb-12 bg-gradient-to-b from-[#EEEEEE] via-[#D4EDFF] to-[#87CCFF]">
+    <div id="berita" className="mt-14 mb-12 bg-gradient-to-b from-white via-[#D4EDFF] to-[#87CCFF] dark:bg-gradient-to-b dark:from-gray-900 dark:via-gray-800 dark:to-gray-700">
       <div className="container pt-[52px]">
         {/* Header Section */}
         <div className="flex justify-center mb-10 max-w-[600px] mx-auto">
-          <h1 data-aos="fade-up" className="text-[37px] font-bold">
+          <h1 data-aos="fade-up" className="text-[37px] font-bold text-gray-900 dark:text-gray-100">
             Berita Terbaru
           </h1>
         </div>
         {/* Body Section */}
         <div className="flex flex-col gap-4">
           <Link to="/beritaUtama">
-            <div className="flex justify-end font-bold gap-2">
+            <div className="flex justify-end font-bold gap-2 text-gray-800 dark:text-gray-100">
               <p>Lihat Selengkapnya</p>
               <img src={iconPanah} alt="Arrow" />
             </div>
@@ -67,8 +67,7 @@ const Berita = () => {
                 data-aos="fade-up"
                 data-aos-delay={data.aosDelay}
                 key={data.id}
-                className="flex flex-col w-full h-[432px] border-[#E7E7E7] border-2 rounded-[20px] p-5 bg-white gap-3"
-              >
+                className="flex flex-col w-full h-[432px] border-[#E7E7E7] dark:border-gray-700 border-2 rounded-[20px] p-5 bg-white dark:bg-gray-800 gap-3">
                 <div className="flex justify-end">
                   <div className="h-[28px] w-[102px] flex justify-center items-center bg-gradient-to-r from-[#FFEEAE] to-[#FDD22F] text-black font-bold py-2 px-4 rounded-[30px] hover:from-yellow-500 hover:to-yellow-600">
                     <p className="font-bold text-[12px]">{data.filter}</p>
@@ -77,8 +76,8 @@ const Berita = () => {
                 <div>
                   <img src={data.img} className="h-[157px] w-[363px] object-cover rounded-[20px]" />
                   <div className="flex flex-col justify-between gap-5 mt-4">
-                    <h3 className="text-[15px] font-bold">{data.title}</h3>
-                    <p className="text-[12px] font-light text-gray-600">{data.Deskription}</p>
+                    <h3 className="text-[15px] font-bold text-gray-900 dark:text-gray-100">{data.title}</h3>
+                    <p className="text-[12px] font-light text-gray-600 dark:text-gray-400">{data.Deskription}</p>
                   </div>
                 </div>
               </div>
@@ -87,10 +86,6 @@ const Berita = () => {
         </div>
       </div>
     </div>
-
-
-
-
   );
 };
 
